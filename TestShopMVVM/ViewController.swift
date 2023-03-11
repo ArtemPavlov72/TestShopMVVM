9e06fc60527066f5840aa454b71b0d517306ed0e
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-03-11 21:55:29 +0500

    Initial Commit

---
+++ TestShopMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestShopMVVM
+//
+//  Created by Артем Павлов on 11.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
